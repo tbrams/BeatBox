6159c7897cf5d5f8299f1d6d4e329b5ea1daa228
tbrams <torben@brams.dk>
2015-10-19 15:09:49 +0700

    All wired up and rotation safe with setRetainFragment(true)

--- app/src/main/java/dk/brams/android/beatbox/BeatBox.java
+++ app/src/main/java/dk/brams/android/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package dk.brams.android.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -10,14 +13,19 @@ import java.util.List;
 
 public class BeatBox {
     private static final String TAG = "BeatBox";
-
+    public static final int MAX_SOUNDS = 5;
     private static final String SOUNDS_FOLDER = "sample_sounds";
 
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+
+        // This is deprecated but we need it for compatibility
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+
         loadSounds();
     }
 
@@ -34,13 +42,36 @@ public class BeatBox {
 
         for (String fileName :
                 soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + fileName;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + fileName;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Could not load sound file: " + fileName, ioe);
+            }
         }
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
         return mSounds;
+    }
+
+    public void play(Sound sound) {
+        Integer soundID = sound.getSoundId();
+        if (soundID == null) {
+            return;
+        }
+        mSoundPool.play(soundID, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void release(){
+        mSoundPool.release();
     }
 }
